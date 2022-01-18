cc8968de2539acf445e88318df3b4a023c9a3ffe
Patrick Maloy <prmaloy@gmail.com>
2022-01-18 15:59:27 -0700

    start category dropdown

--- app/Http/Livewire/SinglePost.php
+++ app/Http/Livewire/SinglePost.php
@@ -4,14 +4,34 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use App\Models\Post;
+use Illuminate\Support\Str;
 
 class SinglePost extends Component
 {
     public Post $post;
+    public $title;
+    public $body;
+
+    public $rules = [
+        'title' => 'required',
+        'body' => 'required'
+    ];
 
     public function mount(Post $post)
     {
         $this->post = $post;
+    }
+
+    public function updatePost($postid)
+    {
+        $this->validate();
+        $newpost = Post::find($postid);
+        $newpost->slug = Str::slug($this->title);
+        $newpost->title = $this->title;
+        $newpost->body = $this->body;
+        $newpost->save();
+
+        return redirect('/posts/' . $postid);
     }
 
     public function render()
--- resources/views/livewire/single-post.blade.php
+++ resources/views/livewire/single-post.blade.php
@@ -1,4 +1,9 @@
 <div>
+    <form wire:submit.prevent='updatePost({{ $post->id }})'>
+        <input wire:model='title' name="title" type="text" placeholder="{{ $post->title }}">
+        <textarea wire:model='body' name="body" cols="30" rows="10" placeholder="{{ $post->body }}"></textarea>
+        <input type="submit" value="Update Post">
+    </form>
     <h1>{{ $post->title }}</h1>
     <p>{{ $post->body }}</p>
     <hr>
